cd5ff2c7dfc5ac5b2b3cc0038bbbd963ff6f09a2
Ewa <ewa.rosset@softwire.com>
2021-11-25 11:15:20 +0000

    Initial commit

---
+++ GasMon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Amazon.Runtime;
+using Amazon.S3;
+using static GasMon.S3Helper.S3Helper;
+
+namespace GasMon
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            ReadObjectDataAsync().Wait();
+        }
+        
+    }
+}
---
+++ GasMon/S3Helper/ReadObjectDataAsync.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace GasMon.S3Helper
+{
+    public class S3Helper
+    {
+        private static readonly string BucketName = "eventprocessing-swapprentices20-locationss3bucket-qu0txg2hhzj2";
+        private static readonly string KeyName = "locations.json";
+        private static readonly RegionEndpoint BucketRegion = RegionEndpoint.EUWest1;
+        private static IAmazonS3 _client;
+        private static BasicAWSCredentials Credentials = new BasicAWSCredentials(Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"), Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"));
+        public static async Task ReadObjectDataAsync()
+        {
+            _client = new AmazonS3Client(Credentials, BucketRegion);
+            string responseBody = "";
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = BucketName,
+                    Key = KeyName
+                };
+                using (GetObjectResponse response = await _client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
+                    string contentType = response.Headers["Content-Type"];
+                    Console.WriteLine("Object metadata, Title: {0}", title);
+                    Console.WriteLine("Content type: {0}", contentType);
+
+                    responseBody = reader.ReadToEnd(); // Now you process the response body.
+                    Console.WriteLine(responseBody);
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                // If bucket or object does not exist
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading object", e.Message);
+            }
+        }
+    }
+}
